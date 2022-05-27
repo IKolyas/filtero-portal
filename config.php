4ed8deecd9f2f71aeea2e4dba3515f97c2b001d0
ik_sunweb <ik@sunweb.pro>
2022-05-27 15:58:42 +0500

    4: Добавляет SingleTone и DataBase классы

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+const DB_CONF = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'a0678406_db',
+    'user' => 'a0678406_db',
+    'password' => 'filteroDb',
+    'charset' => 'utf8'
+];
--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,13 @@
 <?php
-try {
-    $connect = new PDO('mysql:dbname=a0678406_db;host=localhost', 'a0678406_db', 'filteroDb');
-    var_dump($connect);
-} catch (Exception $e) {
-    var_dump($e);
-}
+
+include $_SERVER['DOCUMENT_ROOT'] . "/../vendor/autoload.php";
+
+
+$db = new \app\services\DataBase();
+$allTest = $db->queryOne("SELECT * FROM test", []);
+$allTest2 = $db->queryAll("SELECT * FROM test WHERE id = :id", ['id' => 2]);
+
+var_dump($allTest);
+var_dump($allTest2);
 
 
