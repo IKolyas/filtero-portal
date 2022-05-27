03669a969209bfa94261aeae6f3dfafff111c2c8
ik_sunweb <ik@sunweb.pro>
2022-05-27 17:32:10 +0500

    4: Добавляет SingleTone и DataBase классы

--- public_html/index.php
+++ public_html/index.php
@@ -4,8 +4,13 @@ include $_SERVER['DOCUMENT_ROOT'] . "/../vendor/autoload.php";
 
 
 $db = new \app\services\DataBase();
-$allTest = $db->queryOne("SELECT * FROM test", []);
-$allTest2 = $db->queryAll("SELECT * FROM test WHERE id = :id", ['id' => 2]);
+$allTest = $db->queryOne("SELECT * FROM test  WHERE id = :id", ['id' => 2]);
+$allTest2 = $db->queryAll("SELECT * FROM test", []);
 
-var_dump($allTest);
-var_dump($allTest2);
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+$twig = new Environment($loader);
+
+echo $twig->render('example.html.twig', ['users' => $allTest2]);
