c2539ee9cc76b5e8e01dcb17a413dd62acedc465
ik_sunweb <ik@sunweb.pro>
2022-06-03 16:12:41 +0500

    add renderer

--- base/Application.php
+++ base/Application.php
@@ -5,4 +5,5 @@ namespace app\application;
 class Application
 {
 
+
 }
--- controllers/ActivitiesController.php
+++ controllers/ActivitiesController.php
@@ -4,8 +4,6 @@ namespace app\controllers;
 
 use app\base\Request;
 use app\services\DataBase as DataBase;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
 class ActivitiesController extends AbstractController
 {
