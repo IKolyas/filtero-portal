034b14dbe603267fc269f8cfde3c06eba5d3eb13
Shukrona <shukrona.normatova@mail.ru>
2022-06-07 10:35:01 +0500

    added authControllers

--- controllers/ActivitiesController.php
+++ controllers/ActivitiesController.php
@@ -2,13 +2,8 @@
 
 namespace app\controllers;
 
-<<<<<<< HEAD
-use app\base\Request;
 use app\services\DataBase as DataBase;
-=======
-use app\base\Request,
-    app\services\DataBase as DataBase;
->>>>>>> master
+
 
 class ActivitiesController extends AbstractController
 {
@@ -20,15 +15,5 @@ class ActivitiesController extends AbstractController
 
         echo $this->render('activities\index.html.twig', ['activities' => $activities]);
     }
-
-    // public function actionShow()
-    // {
-    //     $params = (new Request())->getParams();
-    //     if((int) $params) {
-    //         echo "Я активность с id = $params";
-    //     } else {
-    //         echo "Aктивность не найдена";
-    //     }
-    // }
 
 }
