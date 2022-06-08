6cbd7e61797bb9a1458e250f7c6b879406b32164
Eugene Babin <thousandsoftdev@gmail.com>
2022-06-08 18:20:41 +0500

    add favicon

--- controllers/ActivityTypesController.php
+++ controllers/ActivityTypesController.php
@@ -9,7 +9,7 @@ class ActivityTypesController extends AbstractController
     public function actionIndex()
     {
         $db = DataBase::getInstance();
-        $activity_types = $db->queryAll("SELECT * FROM activity_tipes", []);
+        $activity_types = $db->queryAll("SELECT * FROM activity_types", []);
         echo $this->render('activityTypes\index.html.twig', ['activity_types' => $activity_types]);
 
     }
