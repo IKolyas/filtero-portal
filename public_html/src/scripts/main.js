d9f5b600fb1c5e715a16041454e99b54b5b47fbf
centerwebpro <102520204+centerwebpro@users.noreply.github.com>
2022-06-17 16:11:16 +0500

    add 2 select logic js

--- public_html/src/scripts/main.js
+++ public_html/src/scripts/main.js
@@ -31,6 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let idActiveLink = -1;
     links.forEach((link)=> {
       ++idActiveLink;
+      let parentLink = link.parentElement.parentElement
       let idItemLink = idActiveLink;
       let inputLenght = inputs.length;
       let currentIdContent = idItemLink * inputLenght;
@@ -45,25 +46,21 @@ document.addEventListener("DOMContentLoaded", () => {
         let idLink = link.getAttribute('data-id');
         
         inputs.forEach((input)=>{
-          // console.log('input', input.tagName);
           ++idActiveInput;
           if (inputs[idActiveInput].tagName == 'INPUT') {
             inputs[idActiveInput].value = content.innerHTML;
             inputs[idActiveInput].value = idActiveInput;
           }
+
           if (inputs[idActiveInput].classList.contains('js-institute-id')) {
-            // let valueSelect = inputs[idActiveInput].option;;
-            // console.log('--', valueSelect);
             let valueOptions = inputs[idActiveInput].querySelectorAll('option')
             if (valueOptions.length > 0) {
-              // console.log(valueOptions.getAttribute('data-target-institute-id'));
-              let instituteId = link.parentElement
-                .parentElement
-                .querySelector('[data-target-institute-id]')
-                .getAttribute('data-target-institute-id');
+              let instituteId = parentLink
+              .querySelector('[data-target-institute-id]')
+              .getAttribute('data-target-institute-id');
               
-      
               valueOptions.forEach((elem)=>{
+                // console.log('--', elem);
                 if (elem.value != instituteId) {
                   elem.removeAttribute('selected');
                 } else { 
@@ -73,11 +70,34 @@ document.addEventListener("DOMContentLoaded", () => {
             }
           }
 
+          if (inputs[idActiveInput].classList.contains('js-types-id')) {
+            let valueOptions = inputs[idActiveInput].querySelectorAll('option')
+            if (valueOptions.length > 0) {
+              let typesId = parentLink
+              .querySelector('[data-target-type-id]')
+              .getAttribute('data-target-type-id');
+              
+              valueOptions.forEach((elem)=>{
+                if (elem.value != typesId) {
+                  elem.removeAttribute('selected');
+                } else { 
+                  elem.setAttribute('selected', typesId);
+                }
+              });
+            }
+          }
+
           // TODO: da
-          if (isFirstClick && !inputs[idActiveInput].classList.contains('js-institute-id')) {
+          if (
+            isFirstClick && !inputs[idActiveInput].classList.contains('js-institute-id') &&
+            !inputs[idActiveInput].classList.contains('js-types-id')
+            ) {
             startIdCurrentContent = (idActiveInput + currentIdContent);
             inputs[idActiveInput].value = contents[startIdCurrentContent].innerHTML;
-          } else if (!inputs[idActiveInput].classList.contains('js-institute-id')) {
+          } else if (
+            !inputs[idActiveInput].classList.contains('js-institute-id') &&
+            !inputs[idActiveInput].classList.contains('js-types-id')
+          ) {
             startIdCurrentContent = (idActiveInput + currentIdContent);
             inputs[idActiveInput].value = contents[startIdCurrentContent].innerHTML;
           }
