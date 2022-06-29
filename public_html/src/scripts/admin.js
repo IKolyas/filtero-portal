f039463c982dbed4c1bc7f7021b1d65caffced6e
centerwebpro <102520204+centerwebpro@users.noreply.github.com>
2022-06-29 16:49:34 +0500

    Created function disable button submit

--- public_html/src/scripts/admin.js
+++ public_html/src/scripts/admin.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('adminJS');
-
   let btnEdit = document.querySelectorAll('.js-types-link__edit');
   let form = document.getElementById('form-edit');
 
@@ -20,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
           if (buttonSubmit.innerHTML == 'Сохранить') {
               buttonSubmit.innerHTML = 'Добавить';
           };
+          buttonSubmit.disabled = false;
           if (startEdit) {
               form.action = "/admin/create" + form.action.split("/admin/update")[1];
           }
@@ -87,44 +86,90 @@ document.addEventListener("DOMContentLoaded", () => {
       if (btnEdit.length > 0 && formEditFields.length > 0) {
           btnEdit.forEach(btn => {
               btn.addEventListener('click', e => {
-                  clearErrorsNotification();
-                  window.scroll(0, 0);
-                  if (buttonSubmit.innerHTML == 'Добавить') {
-                      buttonSubmit.innerHTML = 'Сохранить';
-                  };
-                  if (!startEdit) {
-                      form.action = "/admin/update" + form.action.split("/admin/create")[1];
-                      startEdit = true;
-                  }
-                  let row = btn.parentElement.parentElement;
-                  addDataToChangeInput(row, formEditFields);
-                  if (formEditFieldsTextarea.length > 0) {
-                      addDataToChangeInput(row, formEditFieldsTextarea);
-                  }
-                  if (formEditFieldsSelect.length > 0) {
-                      addDataToChangeInput(row, formEditFieldsSelect);
-                  }
-                  
-                  // Удаляем из html кнопки редактировать и удалить
-                  let editLineTable = '';
-                  row.querySelectorAll('th').forEach((elem) => {
-                      elem.classList.add("col-1");
-                      editLineTable+=elem.outerHTML;
-                  });
-                  row.querySelectorAll('td').forEach((elem) => {
-                      if (elem.querySelector('i') == null) { // проверяем чтобы не было кнопки  редактировать
-                          editLineTable+=elem.outerHTML;
-                      }
-                  });
+                clearErrorsNotification();
+                window.scroll(0, 0);
+                
+                if (!startEdit) {
+                    form.action = "/admin/update" + form.action.split("/admin/create")[1];
+                    startEdit = true;
+                }
+                let row = btn.parentElement.parentElement;
+                addDataToChangeInput(row, formEditFields);
+                buttonSubmit.disabled = true;
+                enableButtonSubmit(formEditFields, buttonSubmit);
+                if (formEditFieldsTextarea.length > 0) {
+                    addDataToChangeInput(row, formEditFieldsTextarea);
+                    enableButtonSubmit(formEditFieldsTextarea, buttonSubmit);
+                }
+                if (formEditFieldsSelect.length > 0) {
+                    addDataToChangeInput(row, formEditFieldsSelect);
+                    enableButtonSubmit(formEditFieldsSelect, buttonSubmit);
+                }
+                
+                // Удаляем из html кнопки редактировать и удалить
+                let editLineTable = '';
+                row.querySelectorAll('th').forEach((elem) => {
+                    elem.classList.add("col-1");
+                    editLineTable+=elem.outerHTML;
+                });
+                row.querySelectorAll('td').forEach((elem) => {
+                    if (elem.querySelector('i') == null) { // проверяем чтобы не было кнопки  редактировать
+                        editLineTable+=elem.outerHTML;
+                    }
+                });
 
-                  let htmlStartTable = `<span style="color: green">Редактирование:</span><br><table class="table table-striped table-hover align-middle"><tbody>`
-                  let htmlEndTable = `</tbody></table>`;
-                  let htmlRow = editLineTable;
-                  notification.innerHTML = htmlStartTable + htmlRow + htmlEndTable;
+                if (buttonSubmit.disabled || buttonSubmit.innerHTML == 'Добавить') {
+                    buttonSubmit.innerHTML = 'Сохранить';
+                };
+
+                let htmlStartTable = `<span style="color: green">Редактирование:</span><br><table class="table table-striped table-hover align-middle"><tbody>`
+                let htmlEndTable = `</tbody></table>`;
+                let htmlRow = editLineTable;
+                notification.innerHTML = htmlStartTable + htmlRow + htmlEndTable;
               })
           })
       }
+      if (form.hasAttribute('data-errors-form')) {
+        // buttonSubmit.disabled = true;
+
+        // let idEdit = form.getAttribute('data-errors-form')
+
+        // let table = document.querySelectorAll('.activity_list-admin tbody tr');
+
+        // // console.log('---', table);
+        // // console.log('---', table.querySelector('th').innerHTML);
+
+        // table.forEach(row => {
+        //     let rowEdit = row.querySelector('th').innerHTML
+        //     if (rowEdit == idEdit) {
+        //         for (let field of formEditFields) {
+        //             let selector = row.querySelector(`[data-field='${field.name}']`);
+
+        //             console.log('-------', selector.innerHTML, `${field.name}`);
+        //         }
+
+
+        //         console.log('-------');
+        //     }
+        // })
+
+
+
+
+
+
+        // enableButtonSubmit(formEditFields, buttonSubmit);
+        // if (formEditFieldsTextarea.length > 0) {
+        //     enableButtonSubmit(formEditFieldsTextarea, buttonSubmit);
+        // }
+        // if (formEditFieldsSelect.length > 0) {
+        //     enableButtonSubmit(formEditFieldsSelect, buttonSubmit);
+        // }
+
+    
+      }
   }
+
 
   function clearErrorsNotification() {
       if (document.querySelector('.js-errors-notification')) {
@@ -133,5 +178,19 @@ document.addEventListener("DOMContentLoaded", () => {
               element.innerHTML = '';
           })
       }
+  }
+
+  function enableButtonSubmit(incomingValue, buttonSubmit, incomingData = null) {
+    let dataFields = [];
+    for (let i = 0; i < incomingValue.length; i++) {
+        dataFields[incomingValue[i].name] = incomingValue[i].value;
+
+        incomingValue[i].addEventListener("input", () => {
+            buttonSubmit.disabled = false;
+            if (incomingValue[i].value == dataFields[incomingValue[i].name]) {
+                buttonSubmit.disabled = true;
+            }
+        })
+    }
   }
 });
